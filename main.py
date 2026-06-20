dc6eba6ecd62a4cf956c18dda60c58551eaa96dc
SkyKira23 <melao.rossi.23@gmail.com>
2026-06-20 21:23:10 +0200

    Migliorato main.py con programmazione difensiva e controlli anti-crash

--- main.py
+++ main.py
@@ -15,20 +15,38 @@ if __name__ == '__main__':
 
     a = time.time()
     interpreter = BCInterpreter(bytecode, bcinfos, assertions)
+    
     with open(args.inputfile) as f:
         lines = f.readlines()
+        
+        # --- PRIMO PASSO ---
         interpreter.step(stepMode="forward")
         print("Cycle {}".format(interpreter.getCycleCount()))
-        print("Next line to execute: " + lines[interpreter.getCurrentLine()][:-1])
-        interpreter.step(stepMode="into")
-        print("Cycle {}".format(interpreter.getCycleCount()))
-        print("Next line to execute : " + lines[interpreter.getCurrentLine()][:-1])
+        current_line = interpreter.getCurrentLine()
+        
+        if current_line is not None:
+            print("Next line to execute: " + lines[current_line][:-1])
+            
+            # --- SECONDO PASSO (avviene solo se non abbiamo finito) ---
+            interpreter.step(stepMode="into")
+            print("Cycle {}".format(interpreter.getCycleCount()))
+            current_line = interpreter.getCurrentLine()
+            
+            if current_line is not None:
+                print("Next line to execute : " + lines[current_line][:-1])
+
+        # --- ESECUZIONE DEL RESTO DEL CODICE ---
         interpreter.execute(mode="run")
         print("Cycle {}".format(interpreter.getCycleCount()))
         print("Final registers values:")
         print(interpreter.getRegisters())
-    
+        
     deltaTime = time.time() - a
     cycles = interpreter.getCycleCount()
-    cyclesPerSec = cycles / deltaTime
-    print("Time to execute {} instructions : {} ({:.0f} instr/sec)".format(cycles, deltaTime, cyclesPerSec))
+									 
+																											
+    
+    # Previene l'errore di divisione per zero se l'esecuzione Ã¨ quasi istantanea
+    cyclesPerSec = (cycles / deltaTime) if deltaTime > 0 else 0
+    
+    print("Time to execute {} instructions : {:.5f} ({:.0f} instr/sec)".format(cycles, deltaTime, cyclesPerSec))
